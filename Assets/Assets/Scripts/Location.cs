5d404f6dd3133670275cf40c1a7e9d68b4b65056
T Gibson <gibsontn@iu.edu>
2024-04-25 22:00:55 -0400

    loc1-3statsdisplay

--- Assets/Assets/Scripts/Location.cs
+++ Assets/Assets/Scripts/Location.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class Location : MonoBehaviour
 {
-    public void IncreaseStat()
+    public void IncreaseCharStat()
     {
         GlobalInformation.instance.statlist[(int)Stats.Charisma] +=1;
+        GlobalInformation.instance.ProgressTimeOfDay();
+        Leave();
+    }
+
+    public void IncreaseCourStat()
+    {
+        GlobalInformation.instance.statlist[(int)Stats.Courage] +=1;
+        GlobalInformation.instance.ProgressTimeOfDay();
+        Leave();
+    }
+
+    public void IncreaseClevStat()
+    {
+        GlobalInformation.instance.statlist[(int)Stats.Cleverness] +=1;
+        GlobalInformation.instance.ProgressTimeOfDay();
+        Leave();
+    }
+
+    public void IncreaseCreaStat()
+    {
+        GlobalInformation.instance.statlist[(int)Stats.Creativity] +=1;
         GlobalInformation.instance.ProgressTimeOfDay();
         Leave();
     }
