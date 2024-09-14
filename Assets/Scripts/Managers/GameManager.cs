e7ee7824d43120c851cffe94dfc74c5ff01fb16f
O'Keefe, Gregory J <gokeefe@iu.edu>
2024-09-14 11:18:09 -0400

    Update GameManager.cs
    For whatever reason the game doesn't like us not Destroying the game Manager on load, so I've removed this line

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -42,7 +42,6 @@ public class GameManager : MonoBehaviour
         else
         {
             instance = this;
-            DontDestroyOnLoad(this.gameObject);
         }
 
         SetConvoStart();
