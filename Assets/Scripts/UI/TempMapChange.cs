f0ecfdb0c206d6e32f2d2f44387c5661be43a3b2
ShadowDoggo08 <woodbp@iu.edu>
2024-09-21 16:06:14 -0400

    changed order of scenes to add a main screen for tinkerfest

---
+++ Assets/Scripts/UI/TempMapChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TempMapChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ToMapScene()
+    {
+        SceneManager.LoadScene("Map");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
