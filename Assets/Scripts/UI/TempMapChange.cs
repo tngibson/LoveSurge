51d4548b1ad23b97bbfe5b7e92be28e09c0d544e
O'Keefe, Gregory J <gokeefe@iu.edu>
2024-10-02 00:41:22 -0400

    Final Event Changes
    - Updated Ready to Explore The Net? Event with the new system
    - Updated the RandomEventHandler prefab
    - Fixed some merge issues across all of these pushes (Kitchen content got deleted so I had to revert and then redo the reversions since it was less work)
    - Changed the Logo scene once more so that it now takes us to the WakeUp scene instead of the map.
    -- WakeUp is actually the 1st Random Event, but it never actually got shown off since it was replaced by the Map scene and then never placed on the map. Putting this back in the Logo screen allows it to at least be shown, regardless of if it should really be there or not in the future
    - Changed the names of all of the Random Event scriptable objects so that they are more easily identified

--- Assets/Scripts/UI/TempMapChange.cs
+++ Assets/Scripts/UI/TempMapChange.cs
@@ -8,7 +8,7 @@ public class TempMapChange : MonoBehaviour
     // Start is called before the first frame update
     public void ToMapScene()
     {
-        SceneManager.LoadScene("Map");
+        SceneManager.LoadScene("WakeUp");
     }
 
     void Start()
