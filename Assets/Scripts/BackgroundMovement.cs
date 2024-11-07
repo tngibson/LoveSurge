c647639faea5a1393e3fc029e83c7af2209fa6ec
O'Keefe, Gregory J <gokeefe@iu.edu>
2024-11-07 13:50:00 -0500

    Updated Logo
    Updated the Logo scene and script to be more consistent and less jittery

--- Assets/Scripts/BackgroundMovement.cs
+++ Assets/Scripts/BackgroundMovement.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DirectionalMovement : MonoBehaviour
 {
     public float scrollSpeed = 1f; // Adjust this to control the speed of the scrolling
+    public float backgroundWidth; // Set this to the width of the background in world units
+    public float backgroundHeight; // Set this to the height of the background in world units
+
     private Vector3 initialPosition;
 
     private void Start()
@@ -14,17 +15,20 @@ public class DirectionalMovement : MonoBehaviour
 
     private void Update()
     {
-        // Calculate diagonal movement vector
+        // Move the background diagonally based on scroll speed and deltaTime
         Vector3 movement = new Vector3(scrollSpeed, -scrollSpeed, 0) * Time.deltaTime;
-
-        // Move the background
         transform.Translate(movement);
 
-        // Check if the background has moved off the screen
-        if (transform.position.x > Screen.width + initialPosition.x || transform.position.y < -Screen.height + initialPosition.y)
+        // Check if the background has moved off the screen horizontally
+        if (transform.position.x > initialPosition.x + backgroundWidth)
         {
-            // Move the background back to its initial position to create a looping effect
-            transform.position = initialPosition;
+            transform.position = new Vector3(initialPosition.x - backgroundWidth, transform.position.y, transform.position.z);
+        }
+
+        // Check if the background has moved off the screen vertically
+        if (transform.position.y < initialPosition.y - backgroundHeight)
+        {
+            transform.position = new Vector3(transform.position.x, initialPosition.y + backgroundHeight, transform.position.z);
         }
     }
 }
