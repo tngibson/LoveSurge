a3e07dcd038482921e2fcfe301993d411a0db322
Edison Li <liedi@iu.edu>
2026-01-28 12:12:53 -0500

    point checker started

---
+++ Assets/Scripts/Tutorial/PointChecker.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointChecker : MonoBehaviour
+{
+    //[SerilializeField] private ConvoTopic convoTopic;
+    //[SerilializeField] private PlayTutorialDialog dialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<ConvoTopic>()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if convo topic power tier < 0
+        //play dialog
+    }
+}
