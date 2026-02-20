3720489ed05ce54e54c678aac13904bc90c57b2c
Gregory O'Keefe <gokeefe@iu.edu>
2026-02-20 04:15:52 -0500

    Update CalendarManager.cs

--- Assets/Scripts/Managers/CalendarManager.cs
+++ Assets/Scripts/Managers/CalendarManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static LocationManager;
